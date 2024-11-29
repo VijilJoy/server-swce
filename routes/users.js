0d3cd2c1f2511ca5d2beb249637c9f7113afc00a
vijil-swce <projects.swce.in@gmail.com>
2024-11-30 00:28:18 +0530

    final

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,16 @@
 const router = require("express").Router();
 const { authenticateToken } = require("../Authentication/AuthToken");
+const { User } = require("../dbConfigs/dbConnect");
 router.get("/users", authenticateToken, async (req, res) => {
   try {
     dataEntries = await User.find({}); // Return users data
+  } catch (error) {
+    res.status(500).send("Error fetching data: " + error.message);
+  }
+});
+router.get("/users/:id", authenticateToken, async (req, res) => {
+  try {
+    dataEntries = await User.findById(req.params.id); // Return users data
   } catch (error) {
     res.status(500).send("Error fetching data: " + error.message);
   }
